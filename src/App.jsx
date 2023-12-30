05f26b8b7ad1e8343e8950b50cfd1a62047d5131
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-30 12:12:28 +0530

    Add Next And Prev Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
+import { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import people from './data';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(1);
+
+  const { name, job, image, text } = people[index];
+
+  console.log(people.length);
+  function nextPerson() {
+    let temp = (index + 1) % people.length;
+    setIndex(temp);
+  }
+  function prevPerson() {
+    let temp = (index - 1 + people.length) % people.length;
+    setIndex(temp);
+  }
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} className="person-img" alt={name} />
+          <span className="quote-icon">
+            <FaQuoteRight></FaQuoteRight>
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft></FaChevronLeft>
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight></FaChevronRight>
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
